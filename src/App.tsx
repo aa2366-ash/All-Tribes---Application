75a78c3b5d6018e132ce8860c6b8c9872288c8f1
Arvindh <arvindhashok1112@gmail.com>
2021-05-08 01:16:08 +0530

    add chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Router>
+        <Switch>
+          <Route path="/landing"></Route>
+          <Route path="/register"></Route>
+          <Route path="/invite"></Route>
+          <Route path="/login"></Route>
+          <Route path="/forgotpwd"></Route>
+          <Route path="/home"></Route>
+          <Route path="/tribe/:id"></Route>
+        </Switch>
+      </Router>
+    </ChakraProvider>
   );
 }
 
